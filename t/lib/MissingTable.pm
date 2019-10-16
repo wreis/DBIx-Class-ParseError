f6c4e55491edab9911e57d01b0d54f435fffc945
Wallace Reis <wallace@reis.me>
2019-10-16 20:30:46 +0000

    Add failing test for ParseError API

--- t/lib/MissingTable.pm
+++ t/lib/MissingTable.pm
@@ -4,6 +4,8 @@ use strict;
 use warnings;
 use Test::Roo::Role;
 use Try::Tiny;
+use Test::Exception;
+use DBIx::Class::ParseError;
 
 with 'Storage::Setup';
 
@@ -20,16 +22,26 @@ test 'no table foo' => sub {
     my $self = shift;
     ok(my $schema = $self->_schema, 'got schema');
     ok(my $bar = $schema->resultset('Bar')->create({}), 'created Bar');
-    try {
+    my $exception = try {
         $schema->resultset('Foo')->create({
             name => 'Foo',
             is_foo => 1,
             bar => $bar,
         })
     } catch {
-        my $error = $_;
-        like($error, qr/foo/, 'Missing table foo');
+        my $error_str = $_;
+        my $parser = DBIx::Class::ParseError->new(schema => $self->_schema);
+        my $error = $parser->parse($error_str);
+        is($error_str, $error, 'stringfy');
+        isa_ok($error, 'DBIx::Class::Exception', '::ParseError::Error');
+        is($error_str, $error->message, 'same error str in message');
+        is($error->type, 'missing_table', 'error type');
+        is($error->table, 'foo', 'target table');
+        ok(!$error->column, 'no column involved');
+        ok(!$error->value, 'no value');
+        $error;
     };
+    dies_ok { $exception->rethrow };
 };
 
 1;
